5bad76f25e5ffe9c34abe9969aed63f2aae4c67e
Кермикон <vika.elmseeva@mail.ru>
2024-04-19 21:38:56 +0300

    создала проект формы

---
+++ Новая папка/лаба 14 команда ура/лаба 14 команда ура/Form1.cs
@@ -0,0 +1,15 @@
+namespace лаба_14_команда_ура
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
