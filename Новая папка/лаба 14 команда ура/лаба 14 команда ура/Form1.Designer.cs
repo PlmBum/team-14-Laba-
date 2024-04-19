37293cee249aae3fa9a59a8620d1d1d580fdf69e
Кермикон <vika.elmseeva@mail.ru>
2024-04-19 21:53:19 +0300

    добавила кнопочки редактировать и удалить

--- Новая папка/лаба 14 команда ура/лаба 14 команда ура/Form1.Designer.cs
+++ Новая папка/лаба 14 команда ура/лаба 14 команда ура/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(53, 336);
+            button1.Name = "button1";
+            button1.Size = new Size(196, 53);
+            button1.TabIndex = 0;
+            button1.Text = "Редактировать заказ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(518, 336);
+            button2.Name = "button2";
+            button2.Size = new Size(196, 53);
+            button2.TabIndex = 1;
+            button2.Text = "Удалить заказ";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -42,5 +65,8 @@
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
     }
 }
